b1d2da003b1182e755ca23aed248083c39051ed5
Karthik Karanth <karanth.karthik@gmail.com>
2021-05-15 22:50:42 -0600

    test program to ensure cuda is working and we can compile+launch kernels

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
+#include <cassert>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-int main() {
-    std::cout << "Hello from C++" << __cplusplus << "\n";
+#include "grain.h"
+#include "util.h"
 
-    int driverVersion = 0;
-    int runtimeVersion = 0;
-    cuDriverGetVersion(&driverVersion);
-    cudaRuntimeGetVersion(&runtimeVersion);
-    std::cout << "CUDA Driver/Runtime version: " << driverVersion << "/" << runtimeVersion << "\n";
+int main() {
+    const size_t N = 20;
+    float *a{nullptr};
+    float *b{nullptr};
+    float *c{nullptr};
+
+    cudaMallocManaged(&a, N*sizeof(float));
+    cudaMallocManaged(&b, N*sizeof(float));
+    cudaMallocManaged(&c, N*sizeof(float));
+
+    assert(a != nullptr);
+    assert(b != nullptr);
+    assert(c != nullptr);
+
+    std::iota(a, &a[N], 1.0f);
+    std::iota(b, &b[N], 10.0f);
+
+    Grain::add(a, b, c, N);
+
+    std::cout << "a: ";
+    print_arr(a, &a[N]);
+    std::cout << "b: ";
+    print_arr(b, &b[N]);
+    std::cout << "c: ";
+    print_arr(c, &c[N]);
+
+    cudaFree(a);
+    cudaFree(b);
+    cudaFree(c);
 
     return 0;
 }
